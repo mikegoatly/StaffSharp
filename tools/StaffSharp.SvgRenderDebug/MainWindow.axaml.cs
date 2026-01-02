ad815210d8857eb4d3d3442f0f6841d9bb974802
Mike Goatly <mikegoatly@hotmail.com>
2026-01-02 17:05:27 +0000

    Added empty SVG test harness

---
+++ tools/StaffSharp.SvgRenderDebug/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace StaffSharp.SvgRenderDebug
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
