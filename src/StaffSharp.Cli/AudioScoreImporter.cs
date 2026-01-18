afad7104db0162ca4332507e46f1f8b22f2bf322
Mike Goatly <mikegoatly@hotmail.com>
2026-01-18 16:08:34 +0000

    Add ML options configuration to AudioScoreImporter and update ConvertCommand for ML integration

--- src/StaffSharp.Cli/AudioScoreImporter.cs
+++ src/StaffSharp.Cli/AudioScoreImporter.cs
@@ -1,4 +1,6 @@
 using StaffSharp.Audio.Pipeline;
+using StaffSharp.MachineLearning;
+using StaffSharp.MachineLearning.Options;
 using StaffSharp.Notation;
 
 namespace StaffSharp.Cli;
@@ -8,9 +10,34 @@ namespace StaffSharp.Cli;
 /// </summary>
 internal sealed class AudioScoreImporter : IScoreImporter
 {
+    private string? _modelPath;
+    private float? _onsetThreshold;
+    private float? _frameThreshold;
+    private float? _offsetThreshold;
+    private float? _minNoteLength;
+    private bool _useMl;
+
     public string FormatName => "Audio (WAV)";
 
     public IReadOnlyList<string> SupportedExtensions => [".wav"];
+
+    /// <summary>
+    /// Configures ML-based note detection options.
+    /// </summary>
+    public void ConfigureMLOptions(
+        string? modelPath,
+        float? onsetThreshold,
+        float? frameThreshold,
+        float? offsetThreshold,
+        float? minNoteLength)
+    {
+        _useMl = true;
+        _modelPath = modelPath;
+        _onsetThreshold = onsetThreshold;
+        _frameThreshold = frameThreshold;
+        _offsetThreshold = offsetThreshold;
+        _minNoteLength = minNoteLength;
+    }
 
     public async Task<NotationScore> ImportAsync(
         Stream stream,
@@ -22,6 +49,38 @@ internal sealed class AudioScoreImporter : IScoreImporter
             Progress = progress,
             DiagnosticsCollector = progress is not null ? new CliDiagnosticsCollector() : null,
         };
+
+        // Configure ML note detector if requested
+        if (_useMl)
+        {
+            var mlOptions = new MLTranscriptionOptions
+            { 
+                ModelPath = _modelPath,
+            };
+
+            // Don't override the default options unless specified
+            if (_onsetThreshold is { } onsetThreshold)
+            {
+                mlOptions = mlOptions with { OnsetThreshold = onsetThreshold };
+            }
+
+            if (_frameThreshold is { } frameThreshold)
+            {
+                mlOptions = mlOptions with { FrameThreshold = frameThreshold };
+            }
+
+            if (_offsetThreshold is { } offsetThreshold)
+            {
+                mlOptions = mlOptions with { OffsetThreshold = offsetThreshold };
+            }
+
+            if (_minNoteLength is { } minNoteLength)
+            {
+                mlOptions = mlOptions with { MinNoteLengthSeconds = minNoteLength };
+            }
+
+            options.NoteDetector = MLNoteDetector.Create(mlOptions);
+        }
 
         return await AudioPipeline.FromWavAsync(stream, options, cancellationToken);
     }
