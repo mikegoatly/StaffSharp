72d759de86eb80e9cb4cb568cd802b9bcbe20eb2
Mike Goatly <mikegoatly@hotmail.com>
2026-01-21 19:53:57 +0000

    Correct positioning of dot notations

--- src/StaffSharp.Svg/Layout/Model/AugmentationDottedLayoutSymbol.cs
+++ src/StaffSharp.Svg/Layout/Model/AugmentationDottedLayoutSymbol.cs
@@ -9,4 +9,16 @@ internal abstract class AugmentationDottedLayoutSymbol : LayoutSymbol
     public int DotCount { get; set; }
     public List<double> DotXPositions { get; } = [];
     public double DotY { get; set; }
+
+    public override void Offset(double dx, double dy)
+    {
+        base.Offset(dx, dy);
+
+        // Offset dot positions
+        DotY += dy;
+        for (int i = 0; i < DotXPositions.Count; i++)
+        {
+            DotXPositions[i] += dx;
+        }
+    }
 }
